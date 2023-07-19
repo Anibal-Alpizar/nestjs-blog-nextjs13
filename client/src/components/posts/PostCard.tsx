b040bc6028136986e04debddacab38e799be15cf
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-19 12:52:38 -0600

    Next13 Images

--- client/src/components/posts/PostCard.tsx
+++ client/src/components/posts/PostCard.tsx
@@ -1,4 +1,5 @@
 import { Posts } from '../../../types/posts.index';
+import Image from 'next/image';
 
 function PostCard({ post }: { post: Posts }) {
   const {
@@ -11,7 +12,15 @@ function PostCard({ post }: { post: Posts }) {
     <div className="bg-white rounded-lg shadow-md p-6">
       <h1 className="text-2xl font-bold mb-4">{title}</h1>
       <p className="text-lg mb-4">{content}</p>
-      {img && <img className="w-full rounded-lg" src={img} alt={title} />}
+      {img && (
+        <img
+          className="w-full rounded-lg"
+          width={500}
+          height={500}
+          src={img}
+          alt={title}
+        />
+      )}
     </div>
   );
 }
