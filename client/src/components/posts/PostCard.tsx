e00cbaf83d6696ef918c3abd77459a6facc3d835
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-20 20:42:22 -0600

    Next Image fixed

--- client/src/components/posts/PostCard.tsx
+++ client/src/components/posts/PostCard.tsx
@@ -8,19 +8,20 @@ function PostCard({ post }: { post: Posts }) {
     img,
   }: { title: string; content: string; img?: string } = post;
 
+  const defaultImg = 'https://picsum.photos/500/500'; // random image
+
   return (
     <div className="bg-white rounded-lg shadow-md p-6">
       <h1 className="text-2xl font-bold mb-4">{title}</h1>
       <p className="text-lg mb-4">{content}</p>
-      {img && (
-        <img
-          className="w-full rounded-lg"
-          width={500}
-          height={500}
-          src={img}
-          alt={title}
-        />
-      )}
+
+      <Image
+        className="w-full rounded-lg"
+        width={500}
+        height={500}
+        src={img || defaultImg}
+        alt={title}
+      />
     </div>
   );
 }
